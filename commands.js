aa9977a8b97e406a6933a1053db702656f40edb7
Saahil <29069313+SALTx@users.noreply.github.com>
2023-08-03 16:12:37 +0800

    Added help command

--- commands.js
+++ commands.js
@@ -1,19 +1,31 @@
 class Command {
-	constructor(name, description, callback) {
-		this.name = name;
-		this.description = description;
-		this.callback = callback;
-	}
+  constructor(name, description, callback) {
+    this.name = name;
+    this.description = description;
+    this.callback = callback;
+  }
 }
 
 const commands = new Map();
 
 //! Start command
 commands.set(
-	'start',
-	new Command('start', 'Start the bot', (msg) => {
-		bot.sendMessage(msg.chat.id, 'Hello, world!');
-	})
+  "start",
+  new Command("start", "Start the bot", (bot, msg, [args]) => {
+    bot.sendMessage(msg.chat.id, "Hello, world!");
+  })
+);
+
+//! Help command
+commands.set(
+  "help",
+  new Command("help", "Shows list of commands", (bot, msg, [args]) => {
+    let helpMessage = "List of commands:\n";
+    commands.forEach((command) => {
+      helpMessage += `/${command.name} - ${command.description}\n`;
+    });
+    bot.sendMessage(msg.chat.id, helpMessage);
+  })
 );
 
 export default commands;
